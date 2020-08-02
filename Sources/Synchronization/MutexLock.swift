ee9b1eb63d56c3a11acfac9cfce8f21d8c810a46
Bjørn Olav Ruud <mail@bjornruud.net>
2020-08-02 13:46:11 +0200

    Do manual allocation for C types in locks in order to avoid a rare crash.

--- Sources/Synchronization/MutexLock.swift
+++ Sources/Synchronization/MutexLock.swift
@@ -9,26 +9,27 @@
 import Foundation
 
 public final class MutexLock: Lockable {
-    private var mutex: pthread_mutex_t
+    private let mutex: UnsafeMutablePointer<pthread_mutex_t>
 
     public init() {
-        mutex = pthread_mutex_t()
-        pthread_mutex_init(&mutex, nil)
+        mutex = .allocate(capacity: 1)
+        pthread_mutex_init(mutex, nil)
     }
 
     deinit {
-        pthread_mutex_destroy(&mutex)
+        pthread_mutex_destroy(mutex)
+        mutex.deallocate()
     }
 
     public func lock() {
-        pthread_mutex_lock(&mutex)
+        pthread_mutex_lock(mutex)
     }
 
     public func unlock() {
-        pthread_mutex_unlock(&mutex)
+        pthread_mutex_unlock(mutex)
     }
 
     public func tryLock() -> Bool {
-        return pthread_mutex_trylock(&mutex) == 0
+        return pthread_mutex_trylock(mutex) == 0
     }
 }
--- Sources/Synchronization/UnfairLock.swift
+++ Sources/Synchronization/UnfairLock.swift
@@ -10,17 +10,40 @@ import Foundation
 
 @available(iOS 10.0, macOS 10.12, tvOS 10.0, watchOS 3.0, *)
 public final class UnfairLock: Lockable {
-    private var unfairLock = os_unfair_lock()
+    private let unfairLock: os_unfair_lock_t
+
+    public init() {
+        // Perform manual allocation because of how the Swift compiler automatically
+        // allocates memory for C types (in order to avoid a rare crash).
+        //
+        // https://forums.swift.org/t/atomic-property-wrapper-for-standard-library/30468/18
+        //
+        // Philippe_Hausler:
+        // This is a two fold issue; 1) the compiler is completely at liberty to write something
+        // back to the taking of the address of a struct (say to write tombstone values) and 2)
+        // the allocation region may not be heap, in the regards types can be allocated on the
+        // stack if the compiler has knowledge of that type and the lifespan of the usage. So as
+        // it stands the only safe way (and by safe I mean way without a crash rate) is to
+        // allocate a region via something like os_unfair_lock_t.allocate(1) and use the resultant
+        // of that to pass into os_unfair_lock_lock.
+        unfairLock = .allocate(capacity: 1)
+        unfairLock.initialize(to: .init())
+    }
+
+    deinit {
+        unfairLock.deinitialize(count: 1)
+        unfairLock.deallocate()
+    }
 
     public func lock() {
-        os_unfair_lock_lock(&unfairLock)
+        os_unfair_lock_lock(unfairLock)
     }
 
     public func unlock() {
-        os_unfair_lock_unlock(&unfairLock)
+        os_unfair_lock_unlock(unfairLock)
     }
 
     public func tryLock() -> Bool {
-        return os_unfair_lock_trylock(&unfairLock)
+        return os_unfair_lock_trylock(unfairLock)
     }
 }
